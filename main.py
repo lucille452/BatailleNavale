640bbf108246d6025a73a5fa8b794e009347adb0
GagaYaba <gaelle.lanic@yahoo.fr>
2024-06-07 10:16:49 +0200

    first attempts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
+import random
 
 class BatailleNavale:
     def __init__(self, master):
@@ -9,6 +10,8 @@ class BatailleNavale:
         self.place_navires_mode = False
         self.navires_a_placer = [(5, "porte-avions"), (4, "croiseur"), (3, "destroyer"), (3, "sous-marin"), (2, "torpilleur")]
         self.navires_places = []
+        self.navires_adversaire = self.generer_navires_adversaire()  # Positions des navires de l'adversaire
+        self.tirs_effectues = []  # Positions des tirs effectués
         self.orientation = "horizontal"  # Orientation par défaut
 
     def create_grilles(self):
@@ -75,8 +78,10 @@ class BatailleNavale:
         self.navire_var.set('')
 
     def on_button_click(self, parent, row, col):
-        if self.place_navires_mode:
+        if self.place_navires_mode and parent == self.navires_joueur_frame:
             self.place_navire(parent, row, col)
+        elif parent == self.tir_frame:
+            self.tirer(row, col)
         else:
             messagebox.showinfo("Clic", f"Vous avez cliqué sur la case ({row + 1}, {chr(ord('A') + col)})")
 
@@ -120,6 +125,39 @@ class BatailleNavale:
             self.place_navires_mode = False
             messagebox.showinfo("Fin du Placement", "Tous les navires ont été placés.")
             self.place_navires_button.config(state="disabled")
+
+    def tirer(self, row, col):
+        if (row, col) in self.tirs_effectues:
+            messagebox.showinfo("Tir", "Vous avez déjà tiré sur cette case.")
+            return
+
+        self.tirs_effectues.append((row, col))
+        button = self.tir_buttons[row][col]
+        if (row, col) in self.navires_adversaire:
+            button.config(bg="red")
+            messagebox.showinfo("Tir", "Touché !")
+        else:
+            button.config(bg="white")
+            messagebox.showinfo("Tir", "Manqué !")
+
+    def adversaire_tirer(self):
+        while True:
+            row, col = random.randint(0, 9), random.randint(0, 9)
+            if (row, col) not in self.tirs_effectues:
+                self.tirs_effectues.append((row, col))
+                break
+
+        button = self.navires_joueur_buttons[row][col]
+        if (row, col) in self.navires_places:
+            button.config(bg="red")
+            messagebox.showinfo("Alerte", f"L'adversaire a touché votre navire en ({row + 1}, {chr(ord('A') + col)}) !")
+        else:
+            button.config(bg="white")
+            messagebox.showinfo("Alerte", f"L'adversaire a tiré en ({row + 1}, {chr(ord('A') + col)}) et a manqué.")
+
+    def generer_navires_adversaire(self):
+        # Logique pour générer des navires pour l'adversaire (exemple simplifié)
+        return [(random.randint(0, 9), random.randint(0, 9)) for _ in range(5)]
 
 if __name__ == "__main__":
     root = tk.Tk()
