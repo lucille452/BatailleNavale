ef813252ae0ce1f3b8d4d7a428db04d7606b5827
lucille <lucille.cenac@ynov.com>
2024-06-10 18:18:41 +0200

    class navire

---
+++ Class/Navire.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class Etat(Enum):
+    PLACER = 1
+    TOUCHER = 2
+    COULER = 3
+
+
+class Navire:
+    taille = 0
+    etat = Etat.PLACER
+    nbr_toucher = 0
+    image = 'on verra'
+
+    def __init__(self, taille):
+        self.taille = taille
+
+    def change_etat(self):
+        self.etat = Etat.TOUCHER
+        self.nbr_toucher += 1
+        if self.nbr_toucher == self.taille:
+            self.etat = Etat.COULER
+
+    def get_etat(self):
+        return self.etat
+
