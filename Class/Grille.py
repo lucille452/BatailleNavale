60ead2d294893bb7a203bd202da2bda5f063debd
lucille <lucille.cenac@ynov.com>
2024-06-13 16:30:58 +0200

    class Grille

---
+++ Class/Grille.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+
+class Type(Enum):
+    NAVIRES = 1
+    TIRS = 2
+
+
+class Grille:
+    grille = []
+    type_grille = Type.NAVIRES
+
+    def __init__(self, grille, type_grille):
+        self.grille = grille
+        self.type_grille = type_grille
+
+    # placement navire
+    def place_navire_horizontal(self, line, start_column, end_column):
+        for lineGrille in range(len(self.grille) + 1):
+            if lineGrille == line:
+                for column in range(start_column, end_column + 1):
+                    self.grille[line][column] = 1
+
+    def place_navire_vertical(self, column, start_line, end_line):
+        for line in range(start_line, end_line + 1):
+            self.grille[line][column] = 1
+
+    def get_grille(self):
+        return self.grille
+
+    def get_type_grille(self):
+        return self.type_grille
+
+    # istoucher
+    def is_touche(self, position_line, position_column):
+        self.grille[position_line][position_column] = 2
+
+    # iscouler
+    # def is_couler(self):
+
+
+# grille = Grille([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], Type.NAVIRES)
+# grille.place_navire_vertical(0, 0, 2)
+# print(grille.get_grille())
